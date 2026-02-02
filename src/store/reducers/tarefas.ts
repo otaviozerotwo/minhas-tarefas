9f8bc169febea0e1cfc89041d6655e13087aa65f
Otavio <otavio.zerotwo@gmail.com>
2026-02-01 23:05:49 -0300

    feat: add components && Redux

---
+++ src/store/reducers/tarefas.ts
@@ -0,0 +1,47 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import Tarefa from '../../models/Tarefa'
+
+import * as enums from '../../utils/enums/Tarefa'
+
+type TarefasState = {
+  itens: Tarefa[]
+}
+
+const initialState: TarefasState = {
+  itens: [
+    {
+      id: 1,
+      titulo: 'Estudar JavaScript',
+      descricao: 'Estudar JavaScript revendo o exercício do módulo 7',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.CONCLUIDA
+    },
+    {
+      id: 2,
+      titulo: 'Estudar TypeScript',
+      descricao: 'Estudar material de apoio',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.PENDENTE
+    },
+    {
+      id: 3,
+      titulo: 'Estudar Bootstrap',
+      descricao: 'Praticar a construção de uma landing page',
+      prioridade: enums.Prioridade.IMPORTANTE,
+      status: enums.Status.PENDENTE
+    }
+  ]
+}
+
+const tarefaSlice = createSlice({
+  name: 'tarefas',
+  initialState,
+  reducers: {
+    remover: (state, action: PayloadAction<number>) => {
+      state.itens = state.itens.filter((tarefa) => tarefa.id !== action.payload)
+    }
+  }
+})
+
+export const { remover } = tarefaSlice.actions
+export default tarefaSlice.reducer
